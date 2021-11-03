fb9677db79a88d7b4d9537a9dcb1d9526be363cc
mayukhdeb <mayukhmainak2000@gmail.com>
2021-11-03 23:13:20 +0530

    tiny_transformer: add feed forward implementation

---
+++ tiny_transformer/feed_forward.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class FeedForwardLayer(nn.Module):
+    def __init__(self, dim_input: int = 512, dim_feedforward: int = 2048):
+        """Simple feed forward module containing 2 linear layers and a relu in between.
+
+        As mentioned in the paper:
+        'Each of the layers in our encoder and decoder contains a fully connected feed-forward network, 
+        consists of two linear transformations with a ReLU activation in between."
+
+        Args:
+            dim_input (int, optional): [description]. Defaults to 512.
+            dim_feedforward (int, optional): [description]. Defaults to 2048.
+        """
+        super().__init__()
+        self.layers =  nn.Sequential(
+            nn.Linear(dim_input, dim_feedforward),
+            nn.ReLU(),
+            nn.Linear(dim_feedforward, dim_input),
+        )
+
+    def forward(self, x: torch.Tensor): 
+        return self.layers(x)
