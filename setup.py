ea7c85ffa23f8eef8f405daf597f13d3e32bb1fd
Mayukh Deb <53133634+Mayukhdeb@users.noreply.github.com>
2024-04-27 00:58:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tiny-transformer',
     version='0.0.0',
     author='mayukhdeb',
-    author_email='mayukhmainak2000@gmail.com'
+    author_email='mayukhmainak2000@gmail.com',
+    packages=find_packages(),
 )
